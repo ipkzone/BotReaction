26e442374e4393ea85b2f2cefacd3ac15276273c
Ferry Kirdan Agustin <33730041+ipkzone@users.noreply.github.com>
2019-06-24 17:24:09 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+
+//  * @Author : Ferry Kirdan Agustin
+//  * @Script : BotReaction Facebook
+//  * @Version: 3.1
+//  * @Groups : New Phreaker Ababil
+
+//  * INFORMATION TYPE REACTION :
+//  * LIKE  = 1
+//  * LOVE  = 1
+//  * WOW   = 1
+//  * HAHA  = 1
+//  * SAD   = 1
+//  * ANGRY = 1
+
+$user       = "ipkzone.co.id";
+$pass       = "*************";
+$react      = "2";
+$max_status = "50";
+$token      = "EAAAA********";
+
+?>
